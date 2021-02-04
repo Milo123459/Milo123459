d3db2dff2006368c759acb476b426e6726b374dd
Milo <50248166+Milo123459@users.noreply.github.com>
2021-02-04 18:49:41 +0000

    Update update.js

--- update.js
+++ update.js
@@ -1,3 +1,3 @@
 try {
-    console.log(require('child_process').execSync('git config user.name github-actions && git config user.email github-actions@github.com && npx git-go update with stats').toString());
+    console.log(require('child_process').execSync('git config user.name github-actions && git config user.email github-actions@github.com && git add . && git commit -m "update with stats" && git pull && git push').toString());
 } catch {};
